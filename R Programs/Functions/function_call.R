9b0d7a841df782f3068050c0ee6cb4744dbd2cab
emcdevitt10 <125399161+emcdevitt10@users.noreply.github.com>
2024-02-23 11:37:00 -0500

    Update function_call.R

--- R Programs/Functions/function_call.R
+++ R Programs/Functions/function_call.R
@@ -1,7 +1,7 @@
 # R Function Call 
 
 
-functions <- paste0('C:/Users/', Sys.getenv('USERNAME'), '/OneDrive - Rock Creek Group/Research/R Programs/Functions', sep = '')
+functions <- paste0('C:/Users/', Sys.getenv('USERNAME'), '', sep = '')
 
 source(paste0(functions, 'install_packages.R', sep = ''))
 source(paste0(functions, 'load_libraries.R', sep = ''))
@@ -9,4 +9,4 @@ source(paste0(functions, 'api_connections.R', sep = ''))
 source(paste0(functions, 'rcg_style.R', sep = ''))
 
 #To call this file in other programs, use:
-#source(paste0('C:/Users/', Sys.getenv('USERNAME'), '/OneDrive - Rock Creek Group/Research/R Programs/Functions/function_call.R', sep = ''))
+#source(paste0('C:/Users/', Sys.getenv('USERNAME'), 'function_call.R', sep = ''))
